7199ac3872055fc82773a739edf317a83993b265
Uttchii <uchishiba@akane.waseda.jp>
2025-06-10 20:39:09 +0900

    Delete sync.sh

--- sync.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-git add . && git commit -m "Update $(date)" && git push
-echo "✅ GitHubに同期完了！"
-echo "🌐 Google Colab: https://colab.research.google.com/github/Uttchii/learned-image-compression/blob/main/compressai_test.ipynb"
