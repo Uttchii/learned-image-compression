0b9fbcff68ac0bb8e615cfc84508bced522b2200
kensuke <uken0921@gmail.com>
2025-06-10 20:47:33 +0900

    Keep sync.sh with modifications

---
+++ sync.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+git add . && git reset HEAD sync.sh && git commit -m "Update $(date)" && git push
+echo "✅ GitHubに同期完了！"
+echo "🌐 Google Colab: https://colab.research.google.com/github/Uttchii/learned-image-compression/blob/main/compressai_test.ipynb"
